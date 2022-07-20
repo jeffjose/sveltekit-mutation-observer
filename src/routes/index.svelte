a2b2fb438cb7d5ebf9d49d8cd5dfdc02459541ec
Jeffrey Jose <jeffjosejeff@gmail.com>
2022-07-19 21:26:30 -0700

    Initial add

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let element: HTMLElement;
+	let num: number = 10;
+
+	function mutate(node: HTMLElement) {
+		function callback(mutations: MutationRecord[]) {
+			console.log(mutations);
+		}
+
+		const observer = new MutationObserver(callback);
+
+		console.log('Observing', node);
+		observer.observe(node, { childList: true, subtree: true });
+	}
+</script>
+
+<button on:click={() => num++}>Add item</button>
+
+<div bind:this={element} use:mutate>
+	{#each [...Array(num).keys()] as i}
+		<div>
+			{i}
+		</div>
+	{/each}
+</div>
